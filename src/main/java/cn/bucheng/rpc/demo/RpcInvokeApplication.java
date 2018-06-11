9ae99dc26a7ea0ad4bde96e7722598d4b789162c
yinchong <yin.chong@intellif.com>
2018-06-11 14:16:21 +0800

    实现自定义dubbo使用

---
+++ src/main/java/cn/bucheng/rpc/demo/RpcInvokeApplication.java
@@ -0,0 +1,12 @@
+package cn.bucheng.rpc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcInvokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcInvokeApplication.class, args);
+	}
+}
